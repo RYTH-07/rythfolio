ad941a76b2979f342512feea80eb8cf5f4ba1ef0
C Rythan <rythcomputes@gmail.com>
2026-05-15 17:54:23 +0530

    fixed dark mode toggle

---
+++ write_tailwind.py
@@ -0,0 +1,8 @@
+﻿content = '''@import "tailwindcss" source(none);
+@source "../**/*.{js,ts,jsx,tsx}";
+@import "tw-animate-css";
+
+@variant dark (&:where(.dark, .dark *));'''
+with open('src/styles/tailwind.css', 'w', encoding='utf-8') as f:
+    f.write(content)
+print('Tailwind fixed!')
